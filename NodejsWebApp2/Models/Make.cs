8929decffebcc59686425cd1884377403c34b031
leomc3 <leonardomc3@hotmail.com>
2018-07-20 11:43:18 -0300

    Models

---
+++ NodejsWebApp2/Models/Make.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NodejsWebApp2.Models
+{
+
+    public class Make
+    {
+
+        public int Id { get; set; }
+
+        public String nome { get; set; }
+
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+
+
+}
---
+++ NodejsWebApp2/Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NodejsWebApp2.Models
+{
+    /*  */
+    public class Model
+    {
+
+        public int Id { get; set; }
+
+        public String nome { get; set; }
+        
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+    }
+
+
+}
